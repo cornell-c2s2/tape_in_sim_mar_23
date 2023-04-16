012ad1a47b5472796ec1c597b1cd3acf063997c9
ajb497 <ajb497@cornell.edu>
2023-04-15 20:36:18 -0400

    Removed old code | added missing items

--- sim/tape_in_FFT_interconnect/block_test/spi_tc_maker.py
+++ sim/tape_in_FFT_interconnect/block_test/spi_tc_maker.py
@@ -20,28 +20,11 @@ def t( dut,
   dut.spi_min.cs_2    @= cs_3
   dut.spi_min.sclk_2  @= sclk_3
   dut.spi_min.mosi_2  @= mosi_3
-  """
-  dut.spi_min.ms_cs   @= ms_cs
+
+  dut.spi_min.ms_cs @= ms_cs
   dut.spi_min.ms_sclk @= ms_sclk
+  dut.spi_min.ms_mosi @= ms_mosi
   dut.spi_min.ms_miso @= ms_miso
-  dut.sim_eval_combinational()
-  """
-  """
-  dut.spi_min.ms_packet_size_val @= ms_packet_size_val
-  dut.spi_min.ms_packet_size_msg @= ms_packet_size_msg
-  dut.spi_min.ms_cs_val @= ms_cs_val
-  dut.spi_min.ms_cs_msg @= ms_cs_msg
-  dut.spi_min.ms_freq_val @= ms_freq_val
-  dut.spi_min.ms_freq_msg @= ms_freq_msg
-  dut.spi_min.ms_send_rdy @= ms_send_rdy
-  dut.spi_min.ms_recv_val @= ms_recv_val
-  dut.spi_min.ms_recv_msg @= ms_recv_msg
-  dut.spi_min.ms_miso @= ms_miso
-  """
-  #These will be used for the next tape-in
-  #dut.spi_min.ms_mode.rdy @=
-  #dut.spi_min.ms_mode.val @=
-  #dut.spi_min.ms_mode.msg @=
 
 
   if miso != '?':
@@ -189,63 +172,7 @@ def generate_minion_bitwise_test_from_input_array(val_write, val_read, src_msg, 
   
   return output_arr
 
-"""
 
-# Helper function to drive Verilog model and compare test cases
-def t( dut, 
-       cs,    sclk,    mosi,    miso, \
-       cs_2,  sclk_2,  mosi_2,  miso_2, \
-       cs_3,  sclk_3,  mosi_3,  miso_3,
-       ms_cs, ms_sclk, ms_mosi, ms_miso
-       ): #going to be so messy on the screen, must be a better way to do this. 
-
-  # Write input value to input port
-  dut.spi_min.cs      @= cs
-  dut.spi_min.sclk    @= sclk
-  dut.spi_min.mosi    @= mosi
-
-  dut.spi_min.cs_2    @= cs_2
-  dut.spi_min.sclk_2  @= sclk_2
-  dut.spi_min.mosi_2  @= mosi_2
-
-  dut.spi_min.cs_2    @= cs_3
-  dut.spi_min.sclk_2  @= sclk_3
-  dut.spi_min.mosi_2  @= mosi_3
-
-  dut.spi_min.ms_cs   @= ms_cs
-  dut.spi_min.ms_sclk @= ms_sclk
-  dut.spi_min.ms_miso @= ms_miso
-  dut.sim_eval_combinational()
-
-
-  if miso != '?':
-    assert dut.spi_min.miso == miso
-
-
-  if miso_2 != '?':
-    assert dut.spi_min.miso_2 == miso_2
-
-  if miso_3 != '?':
-    assert dut.spi_min.miso_3 == miso_3
-
-
-  if ms_mosi != '?':
-    assert dut.spi_min.ms_mosi == ms_mosi
-
-
-  # Tick simulator one cycle
-  dut.sim_tick()
-"""
-"""
-#Helper funciton which appends values to our SPI vector array. Helps for readability.
-def bitwise_master_input_array_helper(output_arr, cs, sclk, mosi, miso):
-  output_arr[0].append(cs)
-  output_arr[1].append(sclk)
-  output_arr[2].append(mosi)
-  output_arr[3].append(miso)
-
-  return output_arr
-"""
 def generate_master_bitwise_test_from_input_array(val_write, val_read, src_msg, snk_msg, PACKET_SIZE):
   output_arr = [[],[],[],[]]
 
